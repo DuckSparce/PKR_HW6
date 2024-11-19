2be9090a90c685479fff433f740ea92a1ed67a00
Andrii Kityk <kityk.andry@gmail.com>
2024-11-19 17:48:31 +0200

    Add Andrii info

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,9 +4,9 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/Andrii_photo.jpg"} alt="Andrii" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
+          <h2 style={styles.name}>Andrii Kityk</h2>
           <p style={styles.position}>Software engineer</p>
         </div>
       </div>
